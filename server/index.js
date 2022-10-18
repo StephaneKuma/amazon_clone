95fbbec31a1b8bfd008c5885cf6e2a0bea626c76
Kuma Stéphane <kumastephane@gmail.com>
2022-10-18 20:32:57 +0000

    Edit app configuration for deployment

--- server/index.js
+++ server/index.js
@@ -6,7 +6,11 @@ const ordersRouter = require("./routes/orders");
 const productRouter = require("./routes/products");
 const userRouter = require("./routes/user");
 
-const DB = "mongodb://localhost:27017/amazone-clone";
+// const DB = "mongodb://localhost:27017/amazone-clone";
+// const DB =
+//   "mongodb+srv://stekos:<password>@cluster0.otykfs4.mongodb.net/?retryWrites=true&w=majority";
+const DB =
+  "mongodb+srv://stekos:bebechou@cluster0.otykfs4.mongodb.net/?retryWrites=true&w=majority";
 const PORT = 3000;
 
 const app = express();
